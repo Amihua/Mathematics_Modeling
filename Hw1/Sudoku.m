1e712f9f13002bc7eeb4c7801667500d8a51b5c7
Amihua <46915571+Amihua@users.noreply.github.com>
2024-07-07 15:16:16 +0800

    Update Sudoku.m

--- Hw1/Sudoku.m
+++ Hw1/Sudoku.m
@@ -9,13 +9,13 @@ n = 9;
 num_vars = n^3;
 
 % 定义目标函数
-f = ones(num_vars, 1); % 因为目标函数是常数 1，所以这里用一个全 1 的向量表示
+f = ones(num_vars, 1); % 目标函数在这个问题中不重要，因为我们只需要满足约束条件
 
 % 定义等式约束矩阵 Aeq 和右侧向量 beq
 Aeq = [];
 beq = [];
 
-% 约束1: 对于每个 (i,j,k)， x_ijk = 1
+% 约束1: 每行的每个数字恰好出现一次
 for k = 1:n
     for i = 1:n
         row = zeros(1, num_vars);
@@ -27,7 +27,7 @@ for k = 1:n
     end
 end
 
-% 约束2: 对于每个 (i,j,k)， x_ijk = 1
+% 约束2: 每列的每个数字恰好出现一次
 for k = 1:n
     for j = 1:n
         row = zeros(1, num_vars);
@@ -39,7 +39,23 @@ for k = 1:n
     end
 end
 
-% 约束3: 对于每个 (i,j,k)， x_ijk = 1
+% 约束3: 每个小方块内每个数字恰好出现一次
+for k = 1:n
+    for block_row = 0:2
+        for block_col = 0:2
+            row = zeros(1, num_vars);
+            for i = 1:3
+                for j = 1:3
+                    row((k-1)*n^2 + (block_row*3+i-1)*n + (block_col*3+j)) = 1;
+                end
+            end
+            Aeq = [Aeq; row];
+            beq = [beq; 1];
+        end
+    end
+end
+
+% 约束4: 数字1-9恰好在每个格子中出现一次
 for i = 1:n
     for j = 1:n
         row = zeros(1, num_vars);
@@ -51,16 +67,25 @@ for i = 1:n
     end
 end
 
-% 约束4: 对于每个 3x3 小方块， x_ijk = 1
-for k = 1:n
-    for block_row = 0:2
-        for block_col = 0:2
+% 初始条件: 已知的数独格子中的数字
+sudoku_initial = [
+    5 3 0 0 7 0 0 0 0;
+    6 0 0 1 9 5 0 0 0;
+    0 9 8 0 0 0 0 6 0;
+    8 0 0 0 6 0 0 0 3;
+    4 0 0 8 0 3 0 0 1;
+    7 0 0 0 2 0 0 0 6;
+    0 6 0 0 0 0 2 8 0;
+    0 0 0 4 1 9 0 0 5;
+    0 0 0 0 8 0 0 7 9
+];
+
+for i = 1:n
+    for j = 1:n
+        if sudoku_initial(i,j) ~= 0
+            k = sudoku_initial(i,j);
             row = zeros(1, num_vars);
-            for i = 1:3
-                for j = 1:3
-                    row((k-1)*n^2 + (block_row*3+i-1)*n + (block_col*3+j)) = 1;
-                end
-            end
+            row((k-1)*n^2 + (i-1)*n + j) = 1;
             Aeq = [Aeq; row];
             beq = [beq; 1];
         end
@@ -81,7 +106,18 @@ options = optimoptions('intlinprog', 'Display', 'off');
 % 检查是否找到最优解
 if exitflag == 1
     disp('找到最优解:');
-    disp(x);
+    solution = reshape(x, [n, n, n]);
+    solved_sudoku = zeros(n, n);
+    for i = 1:n
+        for j = 1:n
+            for k = 1:n
+                if solution(i, j, k) > 0.99
+                    solved_sudoku(i, j) = k;
+                end
+            end
+        end
+    end
+    disp(solved_sudoku);
 else
     disp('未找到最优解');
 end
